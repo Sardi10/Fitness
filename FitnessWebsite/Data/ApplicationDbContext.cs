18cabaa39d7fc90147f331a3c946ef762985b465
sardi10 <sardikuka@gmail.com>
2024-01-03 21:46:36 +0100

    Set up database and also created Category Table

---
+++ FitnessWebsite/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FitnessWebsite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessWebsite.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
