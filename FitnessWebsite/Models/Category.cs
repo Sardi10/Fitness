f1ac1a0d646b61656761f9f22e0c41f064067fde
sardi10 <sardikuka@gmail.com>
2024-01-11 00:29:21 +0100

    Added some client and server side validations

--- FitnessWebsite/Models/Category.cs
+++ FitnessWebsite/Models/Category.cs
@@ -7,10 +7,12 @@ namespace FitnessWebsite.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Category Name is required")]
+        [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1, 100,ErrorMessage ="Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
